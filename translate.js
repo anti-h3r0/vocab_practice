413407c5a40e9f3765d16a745db5bfe68a7db958
anti-h3r0 <antih3r0.codemonkey@gmail.com>
2022-01-03 23:35:08 -0700

    pruned verb combinations

--- translate.js
+++ translate.js
@@ -22,40 +22,172 @@ async function queryTranslation(from_lang, to_lang, text) {
     url += '&q=' + encodeURI(text);
     url += `&source=${from_lang}`;
     url += `&target=${to_lang}`;
-    
-    let res = await fetch(url, { 
+
+    let res = await fetch(url, {
       method: 'GET',
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json"
       }
     });
-    
+
     let response = undefined;
-    
+
     try {
       response = await res.json();
-    }
-    catch(e) {
+    } catch (e) {
       console.log("There was an error with the translation request: ", e);
     }
-    
+
     let translation = response?.data?.translations[0]?.translatedText;
 
-    if (typeof translation === "object"){
+    if (typeof translation === "object") {
       alert(JSON.stringify(translation, null, 2));
       return "";
-    }
-    else if (!translation) 
+    } else if (!translation)
       return "";
 
     return translation.toLowerCase();
   }
 }
 
+// 5 conjugations
+async function queryNounCombinations(from_lang, to_lang, noun) {
+  let combinations = [];
+
+  if (noun === "") {
+    alert("nothing to translate!");
+    return;
+  }
+
+  if (["a", "e", "i", "o", "u"].includes(noun[0]))
+    for (let a of ["an ", "the ", ""]) 
+      combinations.push(`${a}${noun}`);
+  else
+    for (let a of ["a ", "the ", ""])
+      combinations.push(`${a}${noun}`);
+
+  // plural
+  for (let a of ["the ", ""])
+    combinations.push(`${a}${noun}s`);
 
 
+  let translations = [];
+  for (let sentence of combinations) {
+    translations.push(await queryTranslation(from_lang, to_lang, sentence));
+  }
+
+  let result = {
+    sentences: combinations,
+    translations: translations
+  };
+
+  return result;
+}
+
+// 30 conjugations
 async function queryVerbCombinations(from_lang, to_lang, verb) {
+  let combinations = [];
+  
+  for (let a of ["to ", ""])
+    combinations.push(`${a}${verb}`);
+  
+  for (let a of ["I ", "you ", "they ", "we "])
+    combinations.push(`${a}${verb}`);
+
+  for (let a of ["he ", "she ", "it "])
+    combinations.push(`${a}${verb}s`);
+
+  for (let a of ["I ", "you ", "they ", "he ", "she ", "it ", "we "])
+    for (let b of ["would ", "will ", "shall "])
+      combinations.push(`${a}${b}${verb}`);
+
+
+  let translations = [];
+  for (let sentence of combinations) {
+    translations.push(await queryTranslation(from_lang, to_lang, sentence));
+  }
+
+  let result = {
+    sentences: combinations,
+    translations: translations
+  };
+
+  return result;
+}
+
+// 8 conjugations
+async function queryVerbCombinations_PastTense(from_lang, to_lang, verb) {
+  let combinations = [];
+
+  for (let a of ["I ", "you ", "they ", "he ", "she ", "it ", "we ", ""])
+        combinations.push(`${a}${verb}`);
+
+        
+  let translations = [];
+  for (let sentence of combinations) {
+    translations.push(await queryTranslation(from_lang, to_lang, sentence));
+  }
+
+  let result = {
+    sentences: combinations,
+    translations: translations
+  };
+
+  return result;
+}
+
+
+
+// 30 conjugations
+async function queryVerbCombinations_ToBe(from_lang, to_lang, verb) {
+  let combinations = [];
+
+  combinations.push(`I am ${verb}`);
+  combinations.push(`I was ${verb}`);
+  combinations.push(`I have been ${verb}`);
+  
+  for (let a of ["to be ", ""])
+    combinations.push(`${a}${verb}`);
+  
+  for (let a of ["you ", "they ", "we "])
+    for (let b of ["are ", "were ", "have been "])
+        combinations.push(`${a} ${b} ${verb}`);
+
+  for (let a of ["he ", "she ", "it "])
+    for (let b of ["is ", "was ", "has been "])
+        combinations.push(`${a} ${b} ${verb}s`);
+
+  for (let a of ["I ", "you ", "they ", "he ", "she ", "it ", "we "])
+    for (let b of ["will be "])
+        combinations.push(`${a}${b}${verb}`);
+
+
+
+  let translations = [];
+  for (let sentence of combinations) {
+    translations.push(await queryTranslation(from_lang, to_lang, sentence));
+  }
+
+  let result = {
+    sentences: combinations,
+    translations: translations
+  };
+
+  return result;
+}
+
+
+
+
+// this was the original function which has been pruned from 524 -> 68 conjugations
+// This function also includes negatives, and many non trivial conjugations somone may wish to pick and pull at it, or simply use it outright
+// 
+// This code generates 500 complex sentences from a single verb input. i.e. "<call|run|break|...>" -> "you should not need to <call|run|break|...> it"
+// but it's not perfect. it also generates a few grammatically incorrect sentences. i.e. "call" -> "he call" | "it need to call <it|...>";
+// it can be fixed by breaking the patterns into (I, you, they) & (he, she, it) categories like I did to the smaller patterns above. 
+// But since I don't plan on using this code, I don't feel any need to fix it... sorry. I'll review & merge any pull requests if anyone finds a need for it.
+async function queryVerbCombinations_Comprehensive_PresentTense(from_lang, to_lang, verb) {
   let temp = verb.split(" ");
   if (temp.length != 1) {
     alert(`I cannot translate the verb '${verb}'`);
@@ -111,7 +243,7 @@ async function queryVerbCombinations(from_lang, to_lang, verb) {
     }
   }
 
-  // 72  -> 4: <he|she|it>  <needs to|wants to|_> <verb> <it|_>
+  // 72  -> 5: <he|she|it>  <needs to|wants to|_> <verb> <it|_>
   for (let a of ["he ", "she ", "it "]) {
     for (let c of ["needs to ", "wants to ", ""]) {
       for (let e of [" it", ""]) {
@@ -120,14 +252,14 @@ async function queryVerbCombinations(from_lang, to_lang, verb) {
     }
   }
 
-  // 6   -> 5: <I|you|they> <verb> <it|_>
+  // 6   -> 6: <I|you|they> <verb> <it|_>
   for (let a of ["I ", "you ", "they ", "he ", "she ", "it "]) {
     for (let e of [" it", ""]) {
       combinations.push(`${a}${verb}${e}`);
     }
   }
 
-  // 6   -> 6: <he|she|it> <verb><s> <it|_>
+  // 6   -> 7: <he|she|it> <verb><s> <it|_>
   for (let a of ["he ", "she ", "it "]) {
     for (let e of [" it", ""]) {
       combinations.push(`${a}${verb}s${e}`);
@@ -141,7 +273,7 @@ async function queryVerbCombinations(from_lang, to_lang, verb) {
     }
   }
 
-  // 4   -> 7: <to|_> <verb> <it|_>
+  // 4   -> 9: <to|_> <verb> <it|_>
   for (let a of ["to ", ""]) {
     for (let e of [" it", ""]) {
       combinations.push(`${a}${verb}${e}`);
